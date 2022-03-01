13ca26c22f75764c8aa09c34df5e6901414abc1f
Vishak Raj S Panicker <88788374+viwicodes@users.noreply.github.com>
2022-03-01 12:14:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# PDF to audio reader - in python
+This simple python file will help you to convert pdf to mp3 files.
+
+Literally enabling you to listen to ur pdf files :)
+
+To use this code, clone this repo by
+
+> git clone https://github.com/viwicodes/pdfToMP3
+
+
+Change the pathToFile variable with path to your desired PDF file.
+
+Install dependencies using
+> pip3 install -r requirements.txt
+
+Then run the file using 
+> python3 pdfReader.py
+
+Now wait for some time. It take some time to convert the files. Depends on your system speed and size of pdf file.
+
+
+After running you'll be presented with audio files. Each audio file is a page in your pdf file.
+Feel free to play and listen to it as your comfort.
+
+Thanks For checking out :)
---
+++ pdfReader.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+import pdfplumber, time
+import os
+
+language = 'en'
+pathToFile = 'tst1.pdf'
+
+file = pdfplumber.open(pathToFile)
+pages = file.pages
+
+for i,pg in enumerate(pages):
+    print("Converting page_{}...".format(i))
+    text = pages[i].extract_text()
+    myobj = gTTS(text=text, lang=language, slow=False)
+    myobj.save("page_{}.mp3".format(i))
+    print("Page_{} saved successfully!".format(i))
+
+# Uncomment this section to play audio from terminal
+# Make sure mpg321 is installed to use this feature.
+
+# ask = input("Do you want to read PDF page by page?(Y/n): ")
+# if ask == "Y" or ask == 'y':
+#     # os.system("mpg321 page_0.mp3".format(i))
+#     for j in range(i):
+#         os.system("mpg321 page_{}.mp3".format(j))
